60f0c027bf77bb843202a289df3d203619a0f5e3
Zak Karachiwala <zak.karachiwala@gmail.com>
2023-04-10 16:18:23 +1000

    Add swagger to prod

--- TimeTracker.API/Program.cs
+++ TimeTracker.API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
