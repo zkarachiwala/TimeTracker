0810a398d883073ad6f2e8fac5bb14cfd897fc09
Zak Karachiwala <zak.karachiwala@gmail.com>
2023-04-11 17:50:00 +1000

    Added Migration Script for Azure

---
+++ TimeTracker.API/Scripts/InitialCreate.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [TimeEntries] (
+    [Id] int NOT NULL IDENTITY,
+    [Project] nvarchar(max) NOT NULL,
+    [Start] datetime2 NOT NULL,
+    [End] datetime2 NULL,
+    [DateCreated] datetime2 NOT NULL,
+    [DateUpdated] datetime2 NULL,
+    CONSTRAINT [PK_TimeEntries] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230410072238_InitialCreate', N'7.0.4');
+GO
+
+COMMIT;
+GO
+
