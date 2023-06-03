05171cf9bcbeaac108405d1f925b3894f5a4c08d
Zak Karachiwala <zak.karachiwala@gmail.com>
2023-06-03 16:49:11 +1000

    Added time entry data to time entries page

--- TimeTracker.Client/Program.cs
+++ TimeTracker.Client/Program.cs
@@ -8,4 +8,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
 await builder.Build().RunAsync();
